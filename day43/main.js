57f8ea5535aeca22f06b5ddd5e90be623b272b4e
alex <nba4everwanted@gmail.com>
2022-12-04 09:36:26 +0000

    day43 JS promisses setTimeout API

---
+++ day43/main.js
@@ -0,0 +1,17 @@
+console.log("This is the first line of code in app.js.");
+// Keep the line above as the first line of code
+// Write your code here:
+const usingSTO = () => {
+  console.log(`Running from promise`)
+};
+
+setTimeout(usingSTO, 3000);
+
+
+
+
+
+
+
+// Keep the line below as the last line of code:
+console.log("This is the last line of code in app.js.");
