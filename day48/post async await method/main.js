07d27cfb5e58ccc910152511460a7daab7dea974
alex <nba4everwanted@gmail.com>
2022-12-08 07:57:05 +0000

    day48 Get and POST API using Async and Wait

---
+++ day48/post async await method/main.js
@@ -0,0 +1,43 @@
+// information to reach API
+const apiKey = '7b0471c577cc4f5cade060b81ed03b49';
+const url = 'https://api.rebrandly.com/v1/links';
+
+// Some page elements
+const inputField = document.querySelector('#input');
+const shortenButton = document.querySelector('#shorten');
+const responseField = document.querySelector('#responseField');
+
+// Asynchronous functions
+const shortenUrl = async () => {
+	const urlToShorten = inputField.value;
+  const data = JSON.stringify({destination: urlToShorten});
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      body: data,
+      headers:{
+        'Content-type': 'application/json',
+        'apikey': apiKey
+      }
+    }
+      // add code here
+    );
+		if(response.ok){
+      const jsonResponse = await response.json();
+      renderResponse(jsonResponse);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// Clear page and call Asynchronous functions
+const displayShortUrl = (event) => {
+  event.preventDefault();
+  while(responseField.firstChild){
+    responseField.removeChild(responseField.firstChild);
+  }
+  shortenUrl();
+}
+
+shortenButton.addEventListener('click', displayShortUrl);
