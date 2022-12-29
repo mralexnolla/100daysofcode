cfdf23150ff6631265877511d75aa2fee1b70a66
alex <nba4everwanted@gmail.com>
2022-12-29 14:48:56 +0100

    day73 pass through app useState and useEffect project

---
+++ day73/AddThoughtForm.js
@@ -0,0 +1,38 @@
+import React, {useState} from 'react';
+import { generateId, getNewExpirationTime } from './utilities';
+
+export function AddThoughtForm(props) {
+  const [text, setText] = useState('');
+  
+  const handleTextChange = (e) =>{
+    setText(e.target.value);
+  }
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    const thought = {
+      id: generateId(),
+      text: text,
+      expiresAt: getNewExpirationTime()
+    };
+
+    if(text.length > 0){
+      props.addThought(thought);
+    }
+    
+    setText("");
+  }
+
+  return (
+    <form className="AddThoughtForm" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        aria-label="What's on your mind?"
+        placeholder="What's on your mind?"
+        value={text}
+        onChange={handleTextChange}
+      />
+      <input type="submit" value="Add"   />
+    </form>
+  );
+}
